29fc0e21eda793c48b09f9abcc0e00c5280a96ac
AR-Shahin <mdshahinmije96@gmail.com>
2022-02-10 10:35:27 +0600

    accessor modify

--- app/Models/Crud.php
+++ app/Models/Crud.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,4 +14,11 @@ class Crud extends Model
         return 'slug';
     }
     protected $guarded = [];
+
+    function name(): Attribute
+    {
+        return new Attribute(
+            get: fn ($value) => ucwords($value)
+        );
+    }
 }
