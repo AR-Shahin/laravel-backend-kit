f60f6f56ed1a782cd5d644449978785e92e423c3
AR-Shahin <mdshahinmije96@gmail.com>
2023-02-25 19:20:17 +0600

    version change 9 to 10

--- resources/views/backend/auth/profile.blade.php
+++ resources/views/backend/auth/profile.blade.php
@@ -28,4 +28,31 @@
         </form>
     </div>
 </div>
+<hr>
+<div class="row justify-content-center">
+    <div class="col-md-6 mt-3">
+        <h4 class="text-info text-center">Update Password</h4>
+        <form action="{{ route('admin.profile.update') }}" method="POST">
+            @csrf
+            <div class="my-2">
+                <label for="">Name : </label>
+                <input type="text" class="form-control" name="name" value="{{ $admin->name }}">
+                @error('name')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="my-2">
+                <label for="">Email : </label>
+                <input type="text" class="form-control" name="email" value="{{ $admin->email }}">
+                @error('email')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <div class="my-2">
+                <button class="btn btn-sm btn-success btn-block">Update Profile</button>
+            </div>
+        </form>
+    </div>
+</div>
 @stop
