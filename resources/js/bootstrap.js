9d95af097b8c02bc0fb49c8745b5c096a0b656b9
AR-Shahin <mdshahinmije96@gmail.com>
2023-09-05 20:59:18 +0600

    bootstarp add

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,3 +26,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+
+import '../sass/app.scss';
+import * as bootstrap from 'bootstrap';
+
